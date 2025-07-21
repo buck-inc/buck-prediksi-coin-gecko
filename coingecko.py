470c2e257e90a5839b27a1471fe4958ba5c1ea62
buck-inc <bukhaer@gmail.com>
2025-07-21 13:27:23 +0700

    Add files via upload

---
+++ coingecko.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Halo Dunia! 👋")
+st.subheader("Ini aplikasi AI pertamamu dengan Streamlit 🚀")
+st.write("Selamat datang, kamu berhasil menjalankan Streamlit dari Python!")
+
+st.markdown("---")
+
+st.write("🎯 Yuk lanjut belajar bikin AI prediksi kerusakan mesin atau klasifikasi gambar dari Teachable Machine.")
